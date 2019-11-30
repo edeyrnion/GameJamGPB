46fba626b3589a221ccbc1e7fe736faa92adcd7f
Tim <timschmidt02@yahoo.com>
2019-11-30 14:26:09 +0100

    Added AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Header("General")]
+
+    [Tooltip("Enable AI")]
+    [SerializeField] private bool on = true;
+    [SerializeField] private EnemyAIController aiController;
+
+    [Header("Movement")]
+
+    [Tooltip("Distance until target can be seen. 0=disabled"), Range(0f, 100f)]
+    [SerializeField] private float visionRadius = 100.0f;
+
+    [Tooltip("If the AI should keep a distance to the target")]
+    [SerializeField] private bool keepDistance = false;
+
+    [Tooltip("If runAway==true, the distance to the target the AI tries to keep"), Range(0f, 50f)]
+    [SerializeField] private float keepDistanceAmount = 100.0f;
+
+    [Header("Patrole")]
+
+    [Tooltip("Patrole along waypoints")]
+    [SerializeField] private bool enablePatrole = false; //If true, the AI will make use of the waypoints assigned to it until over-ridden by another functionality.
+
+    [Tooltip("Array of Waypoints")]
+    [SerializeField] private Transform[] waypoints;
+
+    [Tooltip("Patrole along waypoints backwards")]
+    [SerializeField] private bool reversePatrol = true; //if true, patrol units will walk forward and backward along their patrol.
+
+    [Header("Target")]
+
+    [Tooltip("Search for player time in seconds"), Range(0.1f, 10f)]
+    [SerializeField] private float huntingTimer = 5.0f;
+
+    [Tooltip("The target the AI is looking for")]
+    [SerializeField] private Transform target; //The target, or whatever the AI is looking for.
+
+    //private script handled variables
+    private bool initialGo = false; //AI cannot function until it is initialized. //An enhancement to how the AI functions prior to visibly seeing the target. Brings AI to life when target is close, but not visible.
+
+    // Patrole
+    private bool patrolBackwards = false; //used to determine if we're moving forward or backward through the waypoints.
+    private int curPatroleWayPoint = 0; //determines what waypoint we are heading toward.
+
+    // Hunting
+    private Vector3 lastVisTargetPos;
+    private bool playerHasBeenSeen = false;
+    private bool huntingStarted = false;
+    private float lostPlayerTimer;
+
+    void Start()
+    {
+        initialGo = true;
+    }
+
+    void Update()
+    {
+        if (!on || !initialGo)
+        {
+            return;
+        }
+        else
+        {
+            AIFunctionality();
+        }
+    }
+
+    void AIFunctionality()
+    {
+        // no target but target required -> disable AI
+        if ((!target && !enablePatrole) || !on)
+        {
+            return;
+        }
+
+        //Functionality Updates
+        lastVisTargetPos = target.position;
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        Vector3 moveAway = (transform.position - lastVisTargetPos).normalized * keepDistanceAmount;
+
+        // target found
+        if (TargetIsInSight())
+        {
+            // move away from Player
+            if (keepDistance && distanceToTarget < keepDistanceAmount)
+            {
+                MoveTo(moveAway);
+            }
+
+            // target is too far away to attack -> move closer
+            else if (distanceToTarget > aiController.attackRange)
+            {
+                MoveTo(lastVisTargetPos);
+            }
+
+            // atttack if close enough
+            if (distanceToTarget <= aiController.attackRange)
+            {
+                // if (Time.time > lastShotFired + attackTime)
+                aiController.Attack(target);
+            }
+        }
+
+        // search target if AI lost player
+        else if (playerHasBeenSeen && !huntingStarted)
+        {
+            lostPlayerTimer = Time.time + huntingTimer;
+            StartCoroutine(HuntDownTarget(lastVisTargetPos));
+        }
+
+        // patrol
+        else if (enablePatrole)
+        {
+            Patrol();
+        }
+    }
+
+    // standard movement behaviour
+    private void MoveTo(Vector3 destination)
+    {
+        float buffer = 1f;
+        float distance = Vector3.Distance(transform.position, destination);
+        
+        if ((!keepDistance && distance > buffer) || (keepDistance && (distance - buffer) > keepDistanceAmount))
+        {
+            aiController.MoveTo(destination);
+        }
+    }
+
+    // verify AI can see the target
+    private bool TargetIsInSight()
+    {
+        return true;
+
+        Debug.Log("check if target in sight");
+        //determine if the enemy should be doing anything other than standing still
+        //aiEnabled = !((moveableRadius > 0) && (Vector3.Distance(transform.position, target.position) > moveableRadius));
+
+        // check if target is in vision range
+        // visualRadius = 0 -> ignore this check
+        if ((visionRadius > 0) && (Vector3.Distance(transform.position, target.position) > visionRadius))
+        {
+            return false;
+        }
+
+        // check nothing is blocking the line of sight
+        RaycastHit sight;
+
+        if (Physics.Linecast(transform.position, target.position, out sight))
+        {
+            if (!playerHasBeenSeen && sight.transform == target)
+            {
+                playerHasBeenSeen = true;
+            }
+
+            return sight.transform == target;
+        }
+
+        Debug.Log("no target found");
+        return false;
+    }
+
+    // target tracking
+    // called if AI hast lost target -> AI moves to last known position
+    private IEnumerator HuntDownTarget(Vector3 lastPosition)
+    {
+        huntingStarted = true;
+
+        while (huntingStarted)
+        {
+            MoveTo(lastPosition - transform.position);
+
+            // check if AI found target
+            if (TargetIsInSight())
+            {
+                huntingStarted = false;
+                break;
+            }
+
+            // check if AI should cancel search
+            if (Time.time > lostPlayerTimer)
+            {
+                huntingStarted = false;
+                playerHasBeenSeen = false;
+                break;
+            }
+
+            yield return null;
+        }
+
+    }
+
+    // move along waypoints
+    void Patrol()
+    {
+        if (!enablePatrole)
+        {
+            return;
+        }
+
+        MoveTo(CurrentPath());
+
+        float distance = Vector3.Distance(transform.position, CurrentPath());
+
+        // next waypoint if close enough
+        if (distance <= 1f)
+        {
+            NextWaypoint();
+        }
+
+    }
+
+    // current waypoint
+    Vector3 CurrentPath()
+    {
+        return waypoints[curPatroleWayPoint].position;
+    }
+
+    // next waypoint
+    void NextWaypoint()
+    {
+        if (!patrolBackwards)
+        {
+            curPatroleWayPoint++;
+
+            if (curPatroleWayPoint >= waypoints.GetLength(0))
+            {
+                // patrol backwards to first waypoint
+                if (reversePatrol)
+                {
+                    patrolBackwards = true;
+                    curPatroleWayPoint -= 2;
+
+                }
+
+                // go straight to first waypoint
+                else
+                {
+                    curPatroleWayPoint = 0;
+                }
+            }
+        }
+
+        else if (reversePatrol)
+        {
+            curPatroleWayPoint--;
+
+            if (curPatroleWayPoint < 0)
+            {
+                patrolBackwards = false;
+                curPatroleWayPoint = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyAIController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIController : MonoBehaviour
+{
+    [Tooltip("Health"), Range(1f, 100f)]
+    [SerializeField] private float health = 100f;
+
+    [Header("Movement")]
+    [Tooltip("AI movement speed"), Range(0f, 20f)]
+    [SerializeField] private float speed = 10f;
+
+    [Tooltip("The rotation speed during movement")]
+    public float rotationSpeed = 20f; //Rotation during movement modifier. If AI starts spinning at random, increase this value. (First check to make sure it's not due to visual radius limitations)
+
+    [Header("Attack")]
+    [Tooltip("The Distance below the AI will attack"), Range(0.001f, 20f)]
+    [SerializeField] public float attackRange = 20.0f;
+
+    [Tooltip("The speed of attacks in seconds"), Range(0.01f, 5f)]
+    [SerializeField] public float attackSpeed = 20.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        Vector3 direction = destination - transform.position;
+        direction = direction.normalized * speed * Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, destination);
+
+        if (distance > 1f || distance < -1f)
+        {
+            transform.Translate(direction);
+        }
+    }
+
+    public void MoveTowads(Vector3 direction)
+    {
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    internal void Attack(Transform target)
+    {
+        Debug.Log("Attack");
+        // TODO
+    }
+}
