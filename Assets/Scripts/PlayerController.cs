f4af84b423e7cc0dcef5561a33a0485df83f7b14
edeyrnion <matthias.treubrodt@gmail.com>
2019-11-30 03:17:18 +0100

    Added Player ans Camera Controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] [Range(1f, 10f)] private float _walkingSpeed = 8f;
+    [SerializeField] [Range(0f, 1f)] private float _walkingReactionTime = 0.3f;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private float axisX;
+    private float axisY;
+
+    private float movementSpeedX;
+    private float movementSpeedY;
+
+    private Camera _cam;
+
+    private void Start()
     {
-        
+        _cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        axisX = Mathf.SmoothDamp(axisX, Input.GetAxisRaw("Horizontal"), ref movementSpeedX, _walkingReactionTime * 0.2f);
+        axisY = Mathf.SmoothDamp(axisY, Input.GetAxisRaw("Vertical"), ref movementSpeedY, _walkingReactionTime * 0.2f);
+
+        transform.Translate(axisX * _walkingSpeed * Time.deltaTime, 0f, axisY * _walkingSpeed * Time.deltaTime, Space.World);
+
+        if (GetMousePosInWorldSpace(out Vector3 mousePos))
+        {
+            mousePos.y = transform.position.y;
+            transform.LookAt(mousePos);
+        }
+    }
+
+    private bool GetMousePosInWorldSpace(out Vector3 mousePos)
+    {
+        Vector3 mp = Input.mousePosition;
+        var ray = _cam.ScreenPointToRay(mp);
+
+        if (Physics.Raycast(ray, out RaycastHit hitinfo, 100f, _groundLayer))
+        {
+            mousePos = hitinfo.point;
+            return true;
+        }
+
+        mousePos = new Vector3();
+        return false;
     }
 }
---
+++ Assets/Scripts/TopDownCameraController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class TopDownCameraController : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    [Tooltip("Time it takes to interpolate camera position"), Range(1f, 20f)]
+    [SerializeField] private float _positionLerpTime = 10f;
+
+    [Tooltip("Camera angle around world x-axis."), Range(0f, 90f)]
+    [SerializeField] private float _cameraAngle = 50f;
+
+    [Tooltip("Camera height"), Range(0f, 5f)]
+    [SerializeField] private float _cameraPosY = 3f;
+
+    [Tooltip("Camera minimal distance"), Range(0f, 10f)]
+    [SerializeField] private float _distanceMin = 8f;
+
+    [Tooltip("Camera maximal distance"), Range(10f, 20f)]
+    [SerializeField] private float _distanceMax = 12f;
+
+    [SerializeField] private Transform _player;
+
+    private Camera _camera;
+
+    #region CameraState
+    private class CameraState
+    {
+        private float x;
+        private float y;
+        private float z;
+
+        public void SetPosition(Vector3 position)
+        {
+            x = position.x;
+            y = position.y;
+            z = position.z;
+        }
+
+        public void LerpTowards(Vector3 target, float positionLerpPct)
+        {
+            x = Mathf.Lerp(x, target.x, positionLerpPct);
+            y = Mathf.Lerp(y, target.y, positionLerpPct);
+            z = Mathf.Lerp(z, target.z, positionLerpPct);
+        }
+
+        public void UpdateTransform(Transform t)
+        {
+            t.position = new Vector3(x, y, z);
+        }
+    }
+    #endregion
+
+    private readonly CameraState _interpolatingCameraState = new CameraState();
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void OnEnable()
+    {
+        transform.rotation = Quaternion.Euler(_cameraAngle, 0f, 0f);
+        transform.position = GetCameraPosition();
+        _interpolatingCameraState.SetPosition(transform.position);
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(_cameraAngle, 0f, 0f);
+
+        Vector3 desieredCameraPosition = GetCameraPosition();
+
+        _interpolatingCameraState.LerpTowards(desieredCameraPosition, _positionLerpTime * Time.deltaTime);
+
+        _interpolatingCameraState.UpdateTransform(transform);
+    }
+
+    Vector3 GetCameraPosition()
+    {
+        var target = _player.position;
+
+        return target + (Quaternion.Euler(_cameraAngle, 0f, 0f) * (-Vector3.forward * _distanceMax));
+    }
+}
